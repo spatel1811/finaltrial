d18bd1e62ab70d85ce61c3f2747f555fb583bf08
Shivangi J Patel <sjp77@afsconnect2.njit.edu>
2017-12-11 20:44:08 -0500

    todo task displayed

---
+++ models/todo.php
@@ -0,0 +1,23 @@
+<?php
+
+final class todo extends database\model
+{
+    public $id;
+    //public $owneremail;
+    public $ownerid;
+    public $createddate;
+	public $updateddate;
+   // public $duedate;
+    //public $message;
+    public $isdone;
+    protected static $modelName = 'todo';
+
+    public static function getTablename()
+    {
+
+        $tableName = 'todos';
+        return $tableName;
+    }
+}
+
+?>
