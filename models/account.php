a3b435d401a3ed7eed86b0a6ff52b33e90c35564
Shivangi J Patel <sjp77@afsconnect2.njit.edu>
2017-12-17 22:53:36 -0500

    displaying account functionality changed

--- models/account.php
+++ models/account.php
@@ -20,19 +20,16 @@ final class account extends \database\model
     }
 
 
-    //to find a users tasks you need to create a method here.  Use $this->id to get the usersID For the query
+    
     public static function findTasks()
     {
 
-        //I am temporarily putting a findall here but you should add a method to todos that takes the USER ID and returns their tasks.
+        
         $records = todos::findAll();
         print_r($records);
         return $records;
     }
-    //add a method to compare the passwords this is where bcrypt should be done and it should return TRUE / FALSE for login
-
-
-
+    
     public function setPassword($password) {
 
         $password = password_hash($password, PASSWORD_DEFAULT);
