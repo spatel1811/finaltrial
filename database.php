c8c54290de908cd30dc51f36cd8dea15e09e487f
Shivangi J Patel <sjp77@afsconnect2.njit.edu>
2017-12-17 23:09:27 -0500

    code arranged for easy understand

--- database.php
+++ database.php
@@ -1,9 +1,6 @@
 <?php
 
-/*define('DATABASE', 'webgrowt_kwilliam');
-define('USERNAME', 'webgrowt_kwillia');
-define('PASSWORD', '%(w+U$H@$Kez');
-define('CONNECTION', '');*/
+
 
 
 ini_set('display_errors', 'On');
@@ -14,38 +11,38 @@ define('PASSWORD', 'hS1DY7pYO');
 define('CONNECTION', 'sql1.njit.edu');
 class dbConn
 {
-    //variable to hold connection object.
+    
        protected static $db;
-    //private construct - class cannot be instatiated externally.
+    
       private function __construct() 
         {
 
            try 
              {
-               // assign PDO object to db variable
+              
               self::$db = new PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
               self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
              }
 
            catch (PDOException $e)
              {
-              //Output error - would normally log this to error file rather than output to user.
+              
               echo "Connection Error: " . $e->getMessage();
              }
         }
     
-    // get connection function. Static method - accessible without instantiation
+    
     
       public static function getConnection() 
         {
-           //Guarantees single instance, if no connection object exists then create one.
+           
             if (!self::$db) 
             {
-                //new connection object.
+               
                  new dbConn();
             }
         
-          //return connection.
+          
           return self::$db;
         }
 }
